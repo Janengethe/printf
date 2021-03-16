3f996dea75b19eb148845d3cc83b15c8af430943
Janengethe <janengethew@gmail.com>
2021-03-16 18:02:36 +0000

    added the prototypes used in numbers.c and specifiers.c

--- holberton.h
+++ holberton.h
@@ -2,6 +2,9 @@
 #define HOLBERTON_H
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  * struct sym_func - structure for symbols and functions
  *
@@ -11,12 +14,19 @@
 
 struct sym_func
 {
-	char *op;
-	int (*fn)(va_list);
+char *op;
+int (*fn)(va_list);
 };
 typedef struct sym_func sym_fun;
 /* Function prototypes*/
 
+int print_number(va_list args);
+int print_unsgined_number(unsigned int n);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(__attribute__((unused))va_list list);
+int print_integer(va_list list);
+int unsigned_integer(va_list list);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
