6eb0c8b2e5ad6e08188382b65056e1306ac609fd
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-16 12:41:47 +0000

    Second commit

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _printf - prints a formatted string from the main string and other variable
+ * arguments passed to it
+ *
+ * @format: main string
+ * Return : returns the total number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	
--- holberton.h
+++ holberton.h
@@ -2,6 +2,20 @@
 #define HOLBERTON_H
 
 #include <unistd.h>
+/**
+ * struct sym_func - structure for symbols and functions
+ *
+ * @op: Operator
+ * @fn: respective function
+ */
+
+struct sym_func
+{
+	char *op;
+	int (*fn)(va_list);
+};
+typedef struct sym_func sym_fun;
+/* Function prototypes*/
 
 int _putchar(char c);
 int _printf(const char *format, ...);
