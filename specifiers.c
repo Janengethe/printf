800faffc144950f8ededc6823ee6561568c63486
Janengethe <janengethew@gmail.com>
2021-03-20 12:30:48 +0000

    added functions

--- specifiers.c
+++ specifiers.c
@@ -1,6 +1,34 @@
 #include "holberton.h"
 
 /**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+unsigned int i;
+for (i = 0; num > 0; i++)
+{
+num = num / base;
+}
+return (i);
+}
+
+
+/**
+ * _base - sends characters to be written on standard output
+ * @str: String to parse
+ */
+void _base(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+}
+
+/**
  * print_char - Prints character
  * @list: arguments list
  * Return: characters printed
