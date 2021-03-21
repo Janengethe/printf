ae1e4600415323bf20813f82f58fddd9eb5cff59
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-21 19:15:44 +0000

    fixed Betty Style errors

--- print_custom.c
+++ print_custom.c
@@ -7,31 +7,28 @@
 
 int print_custom(va_list list)
 {
-        int i = 0, chars_printed = 0;
-        char c;
+	int i = 0, chars_printed = 0;
+	char c;
 	char *str;
 
 
 	str = va_arg(list, char *);
 
-
-
-        while (str[i])
-        {
-                c = str[i];
-                if ((c > 0 && c  < 32) || c >= 127)
-                {
-                        chars_printed += _putchar('\\');
-                        chars_printed += _putchar('x');
-                        chars_printed += _putchar('0');
+	while (str[i])
+	{
+		c = str[i];
+		if ((c > 0 && c  < 32) || c >= 127)
+		{
+			chars_printed += _putchar('\\');
+			chars_printed += _putchar('x');
+			chars_printed += _putchar('0');
 			chars_printed += _printf("%X", (unsigned int) c);
-
-                }
-                else
-                {
-                        chars_printed += _putchar(c);
-                }
-                i++;
-        }
-        return (chars_printed);
+		}
+		else
+		{
+			chars_printed += _putchar(c);
+		}
+		i++;
+	}
+	return (chars_printed);
 }
